4ed32bfa9d956c774569948fe356351a7ec1317b
Felix Wahls <felixwahls@gmx.de>
2024-06-17 12:22:58 +0200

    added my-skills section

--- src/app/main-content/my-skills/my-skills.component.ts
+++ src/app/main-content/my-skills/my-skills.component.ts
@@ -5,8 +5,49 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './my-skills.component.html',
-  styleUrl: './my-skills.component.scss'
+  styleUrl: './my-skills.component.scss',
 })
 export class MySkillsComponent {
-
+  skills = [
+    {
+      name: 'HTML',
+      img: 'html',
+    },
+    {
+      name: 'CSS',
+      img: 'css',
+    },
+    {
+      name: 'JavaScript',
+      img: 'html',
+    },
+    {
+      name: 'TypeScript',
+      img: 'typescript',
+    },
+    {
+      name: 'Angular',
+      img: 'angular',
+    },
+    {
+      name: 'Rest-API',
+      img: 'api',
+    },
+    {
+      name: 'Firebase',
+      img: 'firebase',
+    },
+    {
+      name: 'Git',
+      img: 'git',
+    },
+    {
+      name: 'Scrum',
+      img: 'scrum',
+    },
+    {
+      name: 'Material Design',
+      img: 'materialDesign',
+    },
+  ];
 }
